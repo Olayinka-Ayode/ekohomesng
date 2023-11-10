5e0a93a7a079a90938727e8d468d1126a26c23d5
Olayinka <theolayinkayodele@gmail.com>
2023-11-10 17:48:51 +0100

    modifies navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -24,9 +24,9 @@ const Navbar = () => {
     return (
         <nav className='flex justify-around items-center relative'>
             <div className="logo-container"><img src={logo} alt="logo" width={logoWidth} /></div>
-            <FontAwesomeIcon icon={icons} className="cursor-pointer" onClick={(e) => {checkIconClick(e)}}/>
-            <div className="list-container w-1/3 items-center absolute top-16 left-12 text-left hidden" id="listNav">
-                <ul className='md:flex justify-around items-center font-playPen'>
+            <FontAwesomeIcon icon={icons} className="cursor-pointer md:hidden" onClick={(e) => {checkIconClick(e)}}/>
+            <div className="list-container w-1/3 items-center absolute md:static top-16 left-12 text-left md:flex hidden md:top-0 md:left-0" id="listNav">
+                <ul className='md:flex justify-around items-center font-playPen w-full'>
                     <li><a href="#">Home</a></li>
                     <li><a href="#about">About</a></li>
                     <li><a href="#referral">Clients</a></li>
